b60165e96d1b059de71b882b674dd4988d321924
Luis Porto <luis.porto.luis@gmail.com>
2020-04-26 18:16:25 -0300

    ajustando responsividade

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ function desvirarTodasCartas(){
     }
 
 }
+function menuLateral(){
+    let width =Number(screen.width);
+    let menu = document.querySelector('.aside');
+    let home = document.querySelector('.home');
+    let main = document.querySelector('#main-content');
+    menu.style = 'margin-left:0px;transition: all 1s';
+    
+    if(width>1000){
+        main.style = 'padding: 5px 5px 0px 280px; width:100%;transition: all 1s';
+    }else{
+        main.style =  'padding:2px; width:100%;transition: all 1s';
+    }
+        
+    
+    
+}
+
+function closeMenu(){
+    let menu = document.querySelector('.aside');
+    let home = document.querySelector('.home');
+    let main = document.querySelector('#main-content');
+    menu.style = 'margin-left:-280px;transition: all 1s';
+    main.style = 'padding-left: -280px;transition: all 1s';
+    home.style = 'width:100%;transition: all 1s';
+}
