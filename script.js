ade7d775b76c2a81bba26e2e8f84396ce56515a1
Luis Porto <luis.porto.luis@gmail.com>
2020-04-26 21:07:27 -0300

    atualizando cores e trabalhando responsividade

--- script.js
+++ script.js
@@ -10,14 +10,9 @@ function virarTodasCartas(){
     
 
 }
-function desvirarTodasCartas(){
-    for (let index = 0; index < 6; index++) {
-        var cartaFrente = document.querySelectorAll('.front')[index];
-        var cartaVerso = document.querySelectorAll('.back')[index];
-        cartaFrente.style.transform = 'perspective(600px) rotateY(0deg)';
-        cartaVerso.style.transform = 'perspective(600px) rotateY(180deg)';   
-    }
-
+function exibirMenu(){
+    let width = Number(screen.width);
+    
 }
 function menuLateral(){
     let width =Number(screen.width);
@@ -30,10 +25,7 @@ function menuLateral(){
         main.style = 'padding: 5px 5px 0px 280px; width:100%;transition: all 1s';
     }else{
         main.style =  'padding:2px; width:100%;transition: all 1s';
-    }
-        
-    
-    
+    }   
 }
 
 function closeMenu(){
