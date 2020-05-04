9e6f281e9efc1104b1848b4e6c8704ee0d0007c6
Luis Porto <luis.porto.luis@gmail.com>
2020-05-03 23:15:18 -0300

    alterando nome das pastas

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-function virarTodasCartas(){
-    var areaCarta = document.getElementById('play');
-    
-        for (let index = 0; index < 6; index++) {
-            var cartaFrente = document.querySelectorAll('.front')[index];
-            var cartaVerso = document.querySelectorAll('.back')[index];
-            cartaFrente.style.transform = 'perspective(600px) rotateY(-180deg)';
-            cartaVerso.style.transform = 'perspective(600px) rotateY(0deg)';   
-        }
-    
-}
+
 function exibirMenu(){
     let width = Number(screen.width);
     
